324e9dd7c077ae166998664c7283562edd56c18a
bmaupin <bmaupin@users.noreply.github.com>
2024-06-12 13:14:49 -0400

    feat: Disable buildings that grant extra agents

--- src/Lua/Quick_Civ_BE.lua
+++ src/Lua/Quick_Civ_BE.lua
@@ -3,7 +3,20 @@ function CanCityConstructBuilding(playerID, cityID, buildingID)
     local city = player:GetCityByID(cityID)
 
     if (PreGame.GetGameOption("GAMEOPTION_NO_ESPIONAGE") == 1) then
+        -- Gives 3 agents
         if buildingID == GameInfoTypes.BUILDING_SPY_AGENCY then
+            return false
+
+        -- Reduces cities max intrigue level by 2
+        elseif buildingID == GameInfoTypes.BUILDING_SURVEILLANCE_WEB then
+            return false
+
+        -- Disable buildings that grant extra agents
+        elseif buildingID == GameInfoTypes.BUILDING_CEL_CRADLE then
+            return false
+        elseif buildingID == GameInfoTypes.BUILDING_FEEDSITE_HUB then
+            return false
+        elseif buildingID == GameInfoTypes.BUILDING_COMMAND_CENTER then
             return false
         end
     end
